cf019568745857bf4a09263f469f0a38b4996445
nikschadowsky <n.s-schadowsky@t-online.de>
2023-08-13 17:03:23 +0200

    refactor: change documentation, restructuring, constants and naming

--- src/main/java/nikschadowsky/engine/window/configuration/SuperWindowConfiguration.java
+++ src/main/java/nikschadowsky/engine/window/configuration/SuperWindowConfiguration.java
@@ -12,39 +12,79 @@ import java.util.Map;
  */
 public class SuperWindowConfiguration {
 
+    public static final SuperWindowConfiguration DEFAULT_CONFIGURATION = new SuperWindowConfigurationBuilder().build();
     private final ConfigurationMap map;
 
-    private String title;
-    private Dimension size;
+    private final String title;
 
-    // private Color background; what is color even?
-    private int tps;
-    private int fps;
+    private final Dimension size;
 
+    // private Color background; what even is color ?
+    private final int tps;
+    private final int fps;
+
+    private static final String KEY_TITLE = "title";
+    private static final String KEY_WIDTH = "width";
+    private static final String KEY_HEIGHT = "height";
+    private static final String KEY_TPS = "tps";
+    private static final String KEY_FPS = "fps";
+    private static final String KEY_BACKGROUND = "background";
+
+    /**
+     * Create a SuperWindowConfiguration using a File at a specific Path.
+     * @param path Path of Configuration File.
+     */
     public SuperWindowConfiguration(String path) {
         this(new SuperWindowConfigurationMap(path));
     }
 
-    private SuperWindowConfiguration(SuperWindowConfigurationMap map) {
+    /**
+     * Create a SuperWindowConfiguration using a SuperWindowConfigurationMap.
+     * @param map SuperWindowConfigurationMap
+     */
+    public SuperWindowConfiguration(SuperWindowConfigurationMap map) {
         this.map = map;
 
-        initFields(map);
+        title = map.getOrDefault(KEY_TITLE);
+
+        size = new Dimension(Integer.parseInt(map.getOrDefault(KEY_WIDTH)), Integer.parseInt(map.getOrDefault(KEY_HEIGHT)));
+        tps = Integer.parseInt(map.getOrDefault(KEY_TPS));
+        fps = Integer.parseInt(map.getOrDefault(KEY_FPS));
+    }
+
+    /**
+     * @return Window Title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @return Window Dimensions
+     */
+    public Dimension getSize() {
+        return new Dimension(size);
+    }
+
+    /**
+     * @return Window Ticks per Second
+     */
+    public int getTicksPerSecond() {
+        return tps;
+    }
+
+    /**
+     * @return  Window Frames per Second
+     */
+    public int getFramesPerSecond() {
+        return fps;
     }
 
 
-    private void initFields(SuperWindowConfigurationMap map) {
-        try {
-            title = map.getOrDefault("title");
-
-            size = new Dimension(Integer.parseInt(map.getOrDefault("width")), Integer.parseInt(map.getOrDefault("height")));
-            tps = Integer.parseInt(map.getOrDefault("tps"));
-            fps = Integer.parseInt(map.getOrDefault("fps"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-
+    /**
+     * Builder class for SuperWindowConfiguration
+     * No need to use an external File to define a Configuration.
+     */
     public static class SuperWindowConfigurationBuilder {
 
         private final Map<String, String> properties;
@@ -54,30 +94,35 @@ public class SuperWindowConfiguration {
         }
 
         public SuperWindowConfigurationBuilder addTitle(String title) {
-            properties.put("title", title);
+            properties.put(KEY_TITLE, title);
             return this;
         }
 
         public SuperWindowConfigurationBuilder addWidthAndHeight(int width, int height) {
-            properties.put("width", String.valueOf(width));
-            properties.put("height", String.valueOf(height));
+            properties.put(KEY_WIDTH, String.valueOf(width));
+            properties.put(KEY_HEIGHT, String.valueOf(height));
             return this;
         }
 
         public SuperWindowConfigurationBuilder addTPS(int tps) {
-            properties.put("tps", String.valueOf(tps));
+            properties.put(KEY_TPS, String.valueOf(tps));
             return this;
         }
 
         public SuperWindowConfigurationBuilder addFPS(int fps) {
-            properties.put("fps", String.valueOf(fps));
+            properties.put(KEY_FPS, String.valueOf(fps));
             return this;
         }
 
+
         public SuperWindowConfigurationBuilder addBackgroundColor(Color c) {
-            throw new NotImplementedException(); // TODO implement this bad boi
+            throw new NotImplementedException("It has not been decided, whether the native Color type will be used or an own implementation"); // TODO implement this bad boi
         }
 
+        /**
+         * Build this Configuration
+         * @return built configuration
+         */
         public SuperWindowConfiguration build() {
             return new SuperWindowConfiguration(new SuperWindowConfigurationMap(properties));
         }
