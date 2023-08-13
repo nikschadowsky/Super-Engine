86c9710b27f3c29b405ba08b04f327fc7c87e71b
nikschadowsky <n.s-schadowsky@t-online.de>
2023-08-13 17:01:31 +0200

    feat: add reason string to constructor

--- src/main/java/nikschadowsky/engine/development/NotImplementedException.java
+++ src/main/java/nikschadowsky/engine/development/NotImplementedException.java
@@ -4,7 +4,7 @@ package nikschadowsky.engine.development;
  * File created on 13.08.2023
  */
 public class NotImplementedException extends UnsupportedOperationException{
-    public NotImplementedException(){
-        super("Sorry, but this method was not implemented yet!");
+    public NotImplementedException(String reason){
+        super("Sorry, but this method was not implemented yet! Reasoning: " + reason);
     }
 }
