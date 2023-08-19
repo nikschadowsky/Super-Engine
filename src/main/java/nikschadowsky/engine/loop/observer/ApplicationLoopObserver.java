d467991936041f7a98ef83e053a5fb6039a8e454
nikschadowsky <n.s-schadowsky@t-online.de>
2023-08-19 19:44:52 +0200

    feat: add Observer Pattern for State Updates of ApplicationLoop

---
+++ src/main/java/nikschadowsky/engine/loop/observer/ApplicationLoopObserver.java
@@ -0,0 +1,10 @@
+package nikschadowsky.engine.loop.observer;
+
+/**
+ * File created on 19.08.2023
+ */
+public interface ApplicationLoopObserver {
+
+    void loopStateUpdate(LoopStateEvent e);
+
+}
