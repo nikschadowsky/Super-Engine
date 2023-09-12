5926f3acebf75d3ae1f2368aa64d2e1fc3dde6c2
nikschadowsky <n.s-schadowsky@t-online.de>
2023-09-12 21:21:40 +0200

    feat: add interfaces for event handling

---
+++ src/main/java/nikschadowsky/engine/event/Event.java
@@ -0,0 +1,7 @@
+package nikschadowsky.engine.event;
+
+/**
+ * File created on 09.09.2023
+ */
+public interface Event {
+}
---
+++ src/main/java/nikschadowsky/engine/event/EventHandler.java
@@ -0,0 +1,11 @@
+package nikschadowsky.engine.event;
+
+/**
+ * File created on 09.09.2023
+ */
+public interface EventHandler<T extends Event> {
+
+    void dequeueAllEvents();
+    void enqueueEvents(T event);
+
+}
