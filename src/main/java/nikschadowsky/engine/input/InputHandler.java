0762815022be5f51b786509c92f62d0d14e7e068
nikschadowsky <n.s-schadowsky@t-online.de>
2023-09-12 21:49:01 +0200

    feat: add implementation for InputHandler

--- src/main/java/nikschadowsky/engine/input/InputHandler.java
+++ src/main/java/nikschadowsky/engine/input/InputHandler.java
@@ -1,7 +1,34 @@
 package nikschadowsky.engine.input;
 
+import nikschadowsky.engine.event.EventHandler;
+import nikschadowsky.engine.event.InputEvent;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * File created on 20.08.2023
  */
-public class InputHandler {
+public class InputHandler implements EventHandler<InputEvent> {
+
+    private final InputUpdatable rootUpdatable;
+
+    private final Queue<InputEvent> inputEventQueue;
+
+    public InputHandler(@NotNull InputUpdatable rootUpdatable) {
+        this.rootUpdatable = rootUpdatable;
+
+        inputEventQueue = new LinkedList<>();
+    }
+
+    public void dequeueAllEvents() {
+        while (!inputEventQueue.isEmpty()) {
+            rootUpdatable.updateInput(inputEventQueue.poll());
+        }
+    }
+
+    public void enqueueEvent(InputEvent e){
+        inputEventQueue.offer(e);
+    }
 }
